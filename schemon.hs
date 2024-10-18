0835675104b72a24d457490fed760c4ba36df361
trithagoras <coreybatchler7@gmail.com>
2024-10-19 09:33:26 +1000

    add basic semantic checks

--- schemon.hs
+++ schemon.hs
@@ -7,7 +7,7 @@ import System.IO (openFile, IOMode (ReadMode), hGetContents)
 data Program a = Message [SPair a] EOF deriving (Show)
 data SType a = TStr | TInt | TFloat | TBool | TChar | TList (SType a) | TObj [SPair a] | TNullable (SType a) | TCustom Identifier deriving (Show)
 data SPair a = SPair Identifier (SType a) deriving (Show)
-newtype Identifier = Identifier String deriving (Show)
+newtype Identifier = Identifier String deriving (Show, Eq)
 data EOF = EOF deriving (Show)
 
 -- Tokens
@@ -19,7 +19,10 @@ data TokenKind
     deriving (Show, Eq)
 
 -- Kind, Line #, Position in line, lexume, Error
-data Token = Token TokenKind Int Int String (Maybe SyntaxError) deriving (Show)
+data Token = Token TokenKind Int Int String (Maybe SyntaxError)
+
+instance Show Token where
+    show (Token kind line col repr err) = repr ++ " at line " ++ show line ++ ":" ++ show col
 
 ----------------------------------- DECODER -----------------------------------
 
@@ -65,7 +68,7 @@ encodeIdentifier (Identifier s) = s
 ----------------------------------- LEXER -----------------------------------
 
 scanTokens :: String -> [Token]
-scanTokens source = scanTokens' source 0 0 0 []
+scanTokens source = scanTokens' source 1 0 0 []
 
 --          source -> line -> start -> current -> acc -> tokens
 scanTokens' :: String -> Int -> Int -> Int -> [Token] -> [Token]
@@ -132,14 +135,6 @@ program stream = do
     current <- expect stream current EOF'
     return (Message ps EOF, current)
 
-pair :: [Token] -> Int -> Either String (SPair a, Int)
-pair stream current = do
-    (ident, current) <- identifier stream current
-    current <- expect stream current Colon
-    (t, current) <- type' stream current
-    return (SPair ident t, current)
-
-
 pairs :: [Token] -> Int -> Either String ([SPair a], Int)
 pairs stream current = do
     (p, current) <- pair stream current
@@ -150,6 +145,12 @@ pairs stream current = do
             return (p:ps, current)
         _ -> return ([p], current)
 
+pair :: [Token] -> Int -> Either String (SPair a, Int)
+pair stream current = do
+    (ident, current) <- identifier stream current
+    current <- expect stream current Colon
+    (t, current) <- type' stream current
+    return (SPair ident t, current)
 
 listType :: [Token] -> Int -> Either String (SType a, Int)
 listType stream current = do
@@ -187,8 +188,8 @@ type' stream current = do
 -- expect :: stream -> current -> expectedToken -> newCurrent
 expect :: [Token] -> Int -> TokenKind -> Either String Int
 expect stream current tokenKind = do
-    Token k _ _ _ _ <- peekAt stream current
-    if k == tokenKind then Right (current + 1) else Left $ "Expected TokenKind " ++ show tokenKind ++ " but received " ++ show k
+    Token k l c r e <- peekAt stream current
+    if k == tokenKind then Right (current + 1) else Left $ "Expected a token of kind " ++ show tokenKind ++ " but received " ++ show (Token k l c r e)
 
 
 identifier :: [Token] -> Int -> Either String (Identifier, Int)
@@ -205,12 +206,61 @@ peekAt stream current
     | otherwise = Right $ stream !! current
 
 
+----------------------------------- SEMANTICS -----------------------------------
+
+-- a newly constructed AST should be verified via semantic checking
+-- an example semantic rule is that top-level messages should only be of object type.
+
+isMessage :: SPair a -> Either String ()
+isMessage (SPair _ (TObj _)) = Right ()
+isMessage p = Left $ "Top level messages must be of type 'object'. Encountered: " ++ show p
+
+isMessages :: [SPair a] -> Either String ()
+isMessages = mapM_ isMessage
+
+visit :: Program a -> Either String ()
+visit prog = visitProgram prog []
+
+visitProgram :: Program a -> [Identifier] -> Either String ()
+visitProgram (Message ps EOF) idents = do
+    _ <- isMessages ps
+    _ <- visitPairs ps idents
+    return ()
+
+visitPairs :: [SPair a] -> [Identifier] -> Either String ()
+visitPairs [] idents = Right ()
+visitPairs (p:ps) idents = do
+    _ <- visitPair p idents
+    visitPairs ps idents
+
+visitPair :: SPair a -> [Identifier] -> Either String ()
+visitPair (SPair ident t) idents = do
+    _ <- visitIdentifier ident idents
+    _ <- visitType t idents
+    return ()
+    
+visitIdentifier :: Identifier -> [Identifier] -> Either String ()
+visitIdentifier (Identifier s) idents = do
+    -- TODO: need to check that this identifier is not already in scope
+    return ()
+
+visitType :: SType a -> [Identifier] -> Either String ()
+visitType (TCustom ident) idents = do
+    _ <- visitIdentifier ident idents
+    if ident `elem` idents then Right () else Left $ "Identifier " ++ show ident ++ " has not been defined before trying to use it as a type."
+visitType (TObj inner) idents = visitPairs inner idents
+visitType (TList inner) idents = visitType inner idents
+visitType _ _ = Right ()
+
+
 ----------------------------------- MAIN -----------------------------------
 
 main = do
     handle <- openFile "test.son" ReadMode
     contents <- hGetContents handle
     case decode contents of
-        Left s -> putStrLn s
-        Right p -> putStrLn $ encode (p :: Program SchemONEncoder)
+        Left s -> putStrLn $ "Syntactic error: " ++ s
+        Right p -> case visit p of
+            Left s -> putStrLn $ "Semantics error: " ++ s
+            Right () -> putStrLn $ encode (p :: Program SchemONEncoder)
 
